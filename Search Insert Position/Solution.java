3831c5c1b61b059de152bb0af7046d5ccdb01be6
rajatarora21 <arorarajat9999@gmail.com>
2019-10-01 18:11:04 -0400

    Array Algorithm

---
+++ Search Insert Position/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        if(target<=nums[0])
+        {
+            return 0;
+        }
+        else if(target==nums[nums.length-1])
+        {
+            return nums.length-1;   
+        }
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==target)
+                    return i;
+            else if(target>nums[i]&&target<nums[i+1])
+                    return i+1;
+        }
+        return nums.length;
+    }
+}
